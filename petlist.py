6a0eb8cec1b36b81ba45aac1949fc8bacfdfd6d5
TharicaDevi <tharica.devi.arumugam@student.mmu.edu.my>
2025-06-19 15:04:23 +0800

    update

--- petlist.py
+++ petlist.py
@@ -1,10 +1,11 @@
-from petsdatabase import init_db, add_pet
+import petsdatabase
 
-init_db()
+def seed_pets():
+    petsdatabase.insert_pet("Milo", "dog1.jpeg", "Dog", "Golden", "Golden Retriever", 1, "available")
+    petsdatabase.insert_pet("Snowy", "cat1.jpeg", "Cat", "White", "Persian", 2, "available")
+    petsdatabase.insert_pet("Hammy", "hamster1.jpeg", "Hamster", "Brown", "Roborovski Dwarf", 2, "available")
+    petsdatabase.insert_pet("Oyen", "cat2.jpeg", "Cat", "Orange", "Persian", 4, "available")
+    petsdatabase.insert_pet("Xixi", "cat3.jpeg", "Cat", "Grey", "Persian", 1, "available")
 
-add_pet("dog1.jpeg", "Dog", "Golden", "Golden retriever", 1)
-add_pet("cat1.jpeg", "Cat", "White", "Persian", 2)
-add_pet("hamster1.jpeg", "Hamster", "Brown", "Roborovski Dwarf ", 2)
-add_pet("cat2.jpeg", "Cat", "Orange", "Persian", 4)
-add_pet("cat3.jpeg", "Cat", "Grey", "Persian", 1)
-exit()
+if __name__ == "__main__":
+    seed_pets()
